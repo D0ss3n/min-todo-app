393b90942c60515679eea707672a97ed663e549e
D0ss3n <carlwilliam.wd@gmail.com>
2026-08-31 01:56:50 +0200

    Uppdatera

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE = 'todo-app-v1';
+const CACHE = 'todo-app-v2';
 const FILES = ['./index.html', './manifest.json', './todo-icon.svg'];
 
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('activate', event => event.waitUntil(
+  caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())
+));
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
