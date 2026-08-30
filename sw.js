9bd97053e72cc4643431e7340f8c87cdf626b2c5
D0ss3n <carlwilliam.wd@gmail.com>
2026-08-31 01:35:34 +0200

    Lägg till todo-sidan

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE = 'todo-app-v1';
+const FILES = ['./todo-lista.html', './manifest.json', './todo-icon.svg'];
+
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
