5fd531e464da3152a2a5a80f11a4541940a17dbd
D0ss3n <carlwilliam.wd@gmail.com>
2026-08-31 02:41:46 +0200

    Uppdatera

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'todo-app-v11';
-const FILES = ['./index.html', './manifest.json', './todo-icon.svg'];
+const CACHE = 'todo-app-v12';
+const FILES = ['./manifest.json', './todo-icon.svg'];
 
 self.addEventListener('install', event => event.waitUntil(
   caches.open(CACHE).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())
@@ -7,7 +7,20 @@ self.addEventListener('install', event => event.waitUntil(
 self.addEventListener('activate', event => event.waitUntil(
   caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())
 ));
+self.addEventListener('message', event => { if (event.data === 'SKIP_WAITING') self.skipWaiting(); });
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const response = await fetch(event.request, { cache:'no-store' });
+        const cache = await caches.open(CACHE); cache.put(event.request, response.clone());
+        return response;
+      } catch {
+        return (await caches.match(event.request)) || caches.match('./index.html');
+      }
+    })());
+    return;
+  }
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
