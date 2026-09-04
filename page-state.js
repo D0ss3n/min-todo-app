fa73fc16c83742ea2fad94a9a2a129fbd316fd2f
D0ss3n <carlwilliam.wd@gmail.com>
2026-09-04 04:25:54 +0200

    .

---
+++ page-state.js
@@ -0,0 +1,17 @@
+/* Kom ihåg den senast öppna informationssidan på just den här enheten. */
+const pageStateKey='todo-current-page';
+const rememberPage=page=>localStorage.setItem(pageStateKey,page);
+
+document.querySelector('#menu-install')?.addEventListener('click',()=>rememberPage('install'));
+document.querySelector('#menu-backup')?.addEventListener('click',()=>rememberPage('backup'));
+document.querySelectorAll('[data-info]').forEach(button=>button.addEventListener('click',()=>rememberPage(button.dataset.info)));
+document.querySelector('#menu-home')?.addEventListener('click',()=>localStorage.removeItem(pageStateKey));
+
+const savedPage=localStorage.getItem(pageStateKey);
+if(savedPage){
+  setTimeout(()=>{
+    if(savedPage==='install')document.querySelector('#menu-install')?.click();
+    else if(savedPage==='backup')document.querySelector('#menu-backup')?.click();
+    else document.querySelector(`[data-info="${savedPage}"]`)?.click();
+  },80);
+}
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'todo-app-v64';
+const CACHE = 'todo-app-v65';
 const FILES = ['./manifest.json', './todo-icon.svg'];
 
 self.addEventListener('install', event => event.waitUntil(
